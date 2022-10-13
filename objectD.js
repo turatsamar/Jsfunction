fb3c6c8fceb23361998b216c9b0d878b86ca44f3
TIMA <64804931+turatsamar@users.noreply.github.com>
2022-10-14 03:56:20 +0900

    Add files via upload

---
+++ objectD.js
@@ -0,0 +1,31 @@
+
+
+let a=3
+let b=1
+
+function sum(a,b){
+    const c=a+b
+    console.log(c)
+}
+sum(a,b)
+ 
+b=4
+a=10
+
+sum(a,b)
+
+let blackCat=4
+let whiteCat=11
+function Cat(blackCat,whiteCat){
+    allCat=blackCat+whiteCat
+    console.log('my home cats sum is =',allCat)
+}
+Cat(blackCat,whiteCat)
+ blackCat=77
+ whiteCat=99
+ Cat(blackCat,whiteCat)
+
+
+
+
+
